36c99c98fa2a5ccb92947e46a02f8cfa3d955562
Bayron <bayron.campaz@jalasoft.com>
2021-07-29 15:06:48 -0500

    Adding database connection

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("category")]
+        public string Category { get; set; }
+
+        [BsonElement("summary")]
+        public string Summary { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("image")]
+        public string Image { get; set; }
+
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+    }
+}
